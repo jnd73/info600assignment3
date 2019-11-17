6f29eeaeaecd05ace00533189a104ed54f8fa664
unknown <jnd73@drexel.edu>
2019-11-17 18:34:46 -0500

    Jenn Zoriel N. De Vera Info600Assignment3 Submission

--- js/main.js
+++ js/main.js
@@ -1,26 +1,75 @@
+
 document.addEventListener('DOMContentLoaded', assignClickHandler)
 
 function assignClickHandler () {
-  document.getElementById('addRec').addEventListener('click', function () {
-    const startYear = document.getElementById('startYear').value
-    if (startYear <= 2000) {
-      window.alert('Incorrect year: ' + startYear)
-      return
-    }
-    const fullName = document.getElementById('fullName').value
-    const major = document.getElementById('major').value
+    
+  $(document).ready(function(){
+    //calling of functions for adding, deleting and loading data
+    $(document).on("click","#deleteRec", delRec);
+    $(document).on("click","#addRec", addingRec);
+    $(document).on("click","#loadData", loadingData);
+   
+  });
 
-    const date = new Date()
-    const time = date.getHours() + ':' + date.getMinutes()
 
-    const newEntry = time + ' - ' + fullName + ', ' + major + ', ' + startYear
+  //functions for loading the data
+  function loadingData(){
+    //empty first the display then proceed to displaying data
+    $("#enteredRecords").empty();
+      $.ajax({
+        method: 'GET',
+        url: '/users',
+        datatype: 'json',
+        success: function(data){
+          $(data.records).each(function(index,fieldName){
+              const date = new Date()
+              const time = date.getHours() + ':' + date.getMinutes()
+              $('#enteredRecords').append(time +' - '+ fieldName.fullName+', '+' '+fieldName.major+', '+fieldName.startYear+
+                '<button value="'+fieldName.id+'" id="deleteRec">Delete</button><br>');
+          });
+        }
 
-    const enteredRecords = document.getElementById('enteredRecords')
-    let newChild = document.createElement('li')
-    newChild.appendChild(document.createTextNode(newEntry))
+      });
+  }
 
-    enteredRecords.appendChild(newChild)
+  //functions for adding records
+  function addingRec(){
+      const startYear = $('#startYear').val();
+      if (startYear < 2000) {
+        window.alert('Incorrect year: ' + startYear)
+        return
+      }
+      $.ajax({
+        url: '/users/',
+        method: 'POST',
+        data: {
+          fullName: $('#fullName').val(),
+          major: $('#major').val(),
+          startYear: $('#startYear').val()
+        }
+      })
+      alert("Adding Record Complete");
+      //reseting the input fields
+      document.getElementById('inputs').reset()
+  }
+  //functions for deleting records
+  function delRec(){
+      const id = $(this).val();
+      $.ajax({
+        url: '/user/'+id,
+        method: 'DELETE',
+        success: function(){
+          alert('Record with an id of '+ id+' has been deleted' );
+          //codes underneath is for refreshing the display
+          $("#enteredRecords").empty();
+          loadingData();
+        },
+        error: function(error){
+          alert(error);
+        }
+      });            
+  }
 
-    document.getElementById('inputs').reset()
-  })
+
+ 
 }
--- server.py
+++ server.py
@@ -36,7 +36,7 @@ def getUsers():
         d = json.load(f)
         return(d)
 
-@app.route('/user/', methods = ['GET'])
+@app.route('/users/', methods = ['POST','GET'])
 def addUser():
     newId = uuid.uuid4().hex[:6]
 
@@ -56,9 +56,9 @@ def addUser():
         # Add a new record to the JSON
         data["records"].append(newUser)
 
-    writeToFile(data, fileName)
+    writeToFile(fileName, data)
 
-@app.route('/user/<user_id>', methods = ['GET'])
+@app.route('/user/<user_id>', methods = ['GET','DELETE'])
 def deleteUser(user_id):
     data = ''
     fileName = 'data/entries.json'
